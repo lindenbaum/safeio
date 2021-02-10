a0afa05bf3578a816e63c080f4bfddca7b7f8df4
Sven Heyll <sven.heyll@gmail.com>
2021-02-10 14:02:41 +0100

    Add initial version of github actions

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { }
+, run ? "bash"
+}:
+(pkgs.buildFHSUserEnv {
+  name = "safeio-build-env";
+  targetPkgs = p:
+    with p;
+    [
+      erlang
+      rebar3
+      gnumake
+      gcc
+      coreutils
+    ];
+  runScript = ''    
+    ${run}
+  '';
+  profile = ''
+    export XDG_CONFIG_HOME=/tmp/safeio-config
+    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    export GIT_SSL_CAINFO="$SSL_CERT_FILE"
+    export LANG=C.UTF-8
+  '';
+
+}).env
+
